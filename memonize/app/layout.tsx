0e805f672182abfa67e394f60266934616f3fcf1
Betsy Zhang <160428307+eastbetsy@users.noreply.github.com>
2025-08-01 04:11:29 +0000

    setup project theme and global css

---
+++ memonize/app/layout.tsx
@@ -0,0 +1,26 @@
+import type {Metadata} from 'next';
+import {Space_Mono} from 'next/font/google'
+import '.globals.css'
+import React from 'react';
+
+const spaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700']
+});
+
+export const metadata: Metadata = {
+  title: 'Memonize',
+  description: 'Every hour is a memo. Memonize your day.'
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={spaceMono.className}>{children}</body>
+    </html>
+  )
+}
